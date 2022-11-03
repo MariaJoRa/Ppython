acd0088edd053aa355b42db91f663525df1edf76
MariaJoRa <115585159+MariaJoRa@users.noreply.github.com>
2022-11-03 16:55:50 -0500

    Add files via upload

---
+++ Actividad.py
@@ -0,0 +1,22 @@
+paises= ['Noruega', 'Egipto', 'Thailandia','Grecia','Escocia','Alemania','República Checa']
+print(paises) #Imprimir lista
+print(sorted(paises)) #Imprimir en orden alfabético
+print(paises) #Imprimir en orden original
+print(sorted(paises, reverse=True)) #Imprimir en orden alfabético inverso
+print(paises)#Imprimir en orden orginal
+paises.reverse()
+print(paises)#Imprimir nuevo orden
+paises.reverse()
+print(paises) #Imprimr lista en orden original
+paises.sort()
+print(paises) #Imprimir lista almacenada en orden alfabético
+paises.sort(reverse=True)
+print(paises) #Imprimir lista almacenada en orden alfabético inverso
+
+
+
+
+
+
+
+
